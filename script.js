8933ae8fc5e233391c106ac794c3c0d9b7a55146
RUCHIR04-R <ruchirgawture@gmail.com>
2025-03-05 15:09:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+    let totalPrice = 0;
+    prices.forEach(price => {
+        totalPrice += parseInt(price.textContent);
+    });
+    
+    const newRow = document.createElement("tr");
+    const newCell = document.createElement("td");
+    
+    newCell.setAttribute("colspan", "2");
+    newCell.textContent = `Total Price: Rs ${totalPrice}`;
+ 
+    newRow.appendChild(newCell);
+    document.querySelector("table").appendChild(newRow);
+    
+    getSumBtn.disabled = true;
 };
 
 getSumBtn.addEventListener("click", getSum);
