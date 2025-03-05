3cbb00da3f7128662f7e82fc703d7446a9462f7e
RUCHIR04-R <ruchirgawture@gmail.com>
2025-03-05 15:17:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,24 +3,32 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  const prices = document.querySelectorAll(".price");
+    const prices = document.querySelectorAll(".price");
+    
     let totalPrice = 0;
+    
     prices.forEach(price => {
         totalPrice += parseInt(price.textContent);
     });
     
-    const newRow = document.createElement("tr");
-    const newCell = document.createElement("td");
-    
-    newCell.setAttribute("colspan", "2");
-    newCell.textContent = `Total Price: Rs ${totalPrice}`;
- 
-    newRow.appendChild(newCell);
-    document.querySelector("table").appendChild(newRow);
+    let totalRow = document.querySelector("#ans");
+    if (!totalRow) {
+       
+        const newRow = document.createElement("tr");
+        const newCell = document.createElement("td");
+     
+        newCell.setAttribute("colspan", "2");
+        newCell.setAttribute("id", "ans");
+        newCell.textContent = `Total Price: Rs ${totalPrice}`;
+       
+        newRow.appendChild(newCell);
+        document.querySelector("table").appendChild(newRow);
+    } else {
+        
+        totalRow.textContent = `Total Price: Rs ${totalPrice}`;
+    }
     
     getSumBtn.disabled = true;
 };
 
 getSumBtn.addEventListener("click", getSum);
-
